c18067c0b5361873e9013d3dce1032f56629c507
Mario Pinzón <U22225732@utp.edu.pe>
2025-11-03 20:58:06 -0500

    autenticación, primera fase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
 import { HeroComponent } from './hero/hero.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
   {
@@ -14,5 +16,7 @@ export const routes: Routes = [
       },
     ],
   },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: MainComponent },
 ];
